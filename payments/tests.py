5cabe78b1d87718aab094d1ad0c410091f9e7f9c
hulahoo <hulahoo@gmail.com>
2020-12-09 10:00:30 +0600

    Added Payment Methods Stripe. And Confirmation via Email

---
+++ payments/tests.py
@@ -0,0 +1,11 @@
+import stripe
+
+stripe.api_key = 'sk_test_51HrzKyG1Bl4N34TFcw02kWTmjSRvscHTulw3NhAjXgqK5dfwLtyOoOBLatBLRhsNT1uJFoZYJid0kzzusxczvO7u00qKYCWb4R'
+
+stripe.PaymentIntent.create(
+          amount=1000,
+            currency='usd',
+              payment_method_types=['Alipay'],
+                receipt_email='jenny.rosen@example.com'
+                )
+
