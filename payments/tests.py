f54abe1fd85ba7c0888306704a0515b76a8409c2
hulahoo <hulahoo@gmail.com>
2020-12-13 20:05:37 +0600

    Bug Fixed

--- payments/tests.py
+++ payments/tests.py
@@ -5,7 +5,7 @@ stripe.api_key = 'sk_test_51HrzKyG1Bl4N34TFcw02kWTmjSRvscHTulw3NhAjXgqK5dfwLtyOo
 stripe.PaymentIntent.create(
           amount=1000,
             currency='usd',
-              payment_method_types=['Alipay'],
-                receipt_email='jenny.rosen@example.com'
+              payment_method_types=['card'],
+                receipt_email='jenny.rosen@example.com',
                 )
 
--- payments/urls.py
+++ payments/urls.py
@@ -1,24 +1,24 @@
-# from sys import path
-
-from django.conf.urls import url
-from django.urls import path
-
-from .views import (
-    add_to_cart,
-    delete_from_cart,
-    order_details,
-    checkout,
-    update_transaction_records,
-    success
-)
-
-app_name = 'shopping_cart'
-
-urlpatterns = [
-    path('add-to-cart/<int:id>/', add_to_cart, name="add_to_cart"),
-    path('order-summary/', order_details, name="order_summary"),
-    path('success/', success, name='purchase_success'),
-    path('item/delete/(?P<item_id>[-\w]+)/', delete_from_cart, name='delete_item'),
-    path('checkout/$', checkout, name='checkout'),
-    path('update-transaction/(?P<token>[-\w]+)/', update_transaction_records, name='update_records'),
-]
+# # from sys import path
+#
+# from django.conf.urls import url
+# from django.urls import path
+#
+# from .views import (
+#     add_to_cart,
+#     delete_from_cart,
+#     order_details,
+#     checkout,
+#     update_transaction_records,
+#     success
+# )
+#
+# app_name = 'shopping_cart'
+#
+# urlpatterns = [
+#     path('add-to-cart/<int:id>/', add_to_cart, name="add_to_cart"),
+#     path('order-summary/', order_details, name="order_summary"),
+#     path('success/', success, name='purchase_success'),
+#     path('item/delete/(?P<item_id>[-\w]+)/', delete_from_cart, name='delete_item'),
+#     path('checkout/$', checkout, name='checkout'),
+#     path('update-transaction/(?P<token>[-\w]+)/', update_transaction_records, name='update_records'),
+# ]
